901056c78f9d0e1759c7c2e8828c0aafb4a7134e
kshired <shiroed1211@naver.com>
2022-02-20 22:29:11 +0900

    [ADD] Jwt 기반코드 추가

---
+++ src/main/kotlin/com/kshired/collaborwriting/filter/CustomAuthFilter.kt
@@ -0,0 +1,28 @@
+package com.kshired.collaborwriting.filter
+
+import com.kshired.collaborwriting.utils.JwtUtil
+import org.springframework.web.filter.GenericFilterBean
+import javax.servlet.FilterChain
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+
+class CustomAuthFilter : GenericFilterBean() {
+    companion object {
+        private const val AUTHORIZATION = "Authorization";
+    }
+
+
+    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+        val httpServletRequest = request as HttpServletRequest
+        val authorizationHeader: String? = httpServletRequest.getHeader(AUTHORIZATION)
+
+        if(authorizationHeader.isNullOrBlank()){
+            TODO("throw error")
+        }
+
+        chain.doFilter(request, response)
+    }
+
+
+}
---
+++ src/main/kotlin/com/kshired/collaborwriting/utils/JwtUtil.kt
@@ -0,0 +1,55 @@
+package com.kshired.collaborwriting.utils
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
+import java.nio.charset.StandardCharsets
+import java.security.Key
+import java.util.*
+import javax.crypto.spec.SecretKeySpec
+
+class JwtUtil {
+    companion object {
+        private const val SECRET_KEY = "thisIsSecret"
+        fun generateAccessToken(userId: String): String {
+            val header = hashMapOf<String, Any>()
+            val payload = hashMapOf<String, Any>()
+
+            header["typ"] = "JWT"
+            header["alg"] = "HS256"
+
+            payload["userId"] = userId
+
+            val expiredTime = 1000L * 60L * 60L
+
+            return Jwts.builder()
+                .setHeader(header)
+                .setClaims(payload)
+                .setSubject("user-access")
+                .setExpiration(Date(Date().time + expiredTime))
+                .signWith(this.getKey(), SignatureAlgorithm.HS256)
+                .compact()
+        }
+
+        fun generateRefreshToken(userId: String): String {
+            val header = hashMapOf<String, Any>()
+
+            header["typ"] = "JWT"
+            header["alg"] = "HS256"
+
+            val expiredTime = 1000L * 60L * 60L * 24L * 14L
+
+            return Jwts.builder()
+                .setHeader(header)
+                .setSubject("user-refresh")
+                .setExpiration(Date(Date().time + expiredTime))
+                .signWith(this.getKey(), SignatureAlgorithm.HS256)
+                .compact()
+        }
+
+        private fun getKey(): Key{
+            return Keys.hmacShaKeyFor(SECRET_KEY.toByteArray(StandardCharsets.UTF_8))
+        }
+    }
+}
+
