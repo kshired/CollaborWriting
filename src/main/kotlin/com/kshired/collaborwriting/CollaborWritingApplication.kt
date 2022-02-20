aeddbc295efb2c7cec4c45bf8e5e63156029beeb
kshired <shiroed1211@naver.com>
2022-02-20 12:36:42 +0900

    [INIT]

---
+++ src/main/kotlin/com/kshired/collaborwriting/CollaborWritingApplication.kt
@@ -0,0 +1,11 @@
+package com.kshired.collaborwriting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CollaborWritingApplication
+
+fun main(args: Array<String>) {
+    runApplication<CollaborWritingApplication>(*args)
+}
---
+++ src/test/kotlin/com/kshired/collaborwriting/CollaborWritingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kshired.collaborwriting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CollaborWritingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
